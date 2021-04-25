bff688cce8a0c249028c8fd2b5002065a611878f
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-25 00:14:15 -0400

    added logic to local storage get and set items

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+var saveBtn = $('.row').children('.saveBtn')
+// console.log(saveBtn)
 
 //=====================  Displays current day  =====================
 //=======================  in the jumbotron  =======================
@@ -11,15 +12,30 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 for (var index = 8; index < 17; index++) {
     if (index < 12) {
-        $('.row').children('.hour').eq(index-8).text(`${index} AM`)
+        $('.row').children('.hour').eq(index - 8).text(`${index} AM`)
+        $('.row').children('.saveBtn').eq(index - 8).attr('id', `btn-${index - 7}`)
     } else {
-        $('.row').children('.hour').eq(index-8).text(`${index} PM`)
+        $('.row').children('.hour').eq(index - 8).text(`${index} PM`)
+        $('.row').children('.saveBtn').eq(index - 8).attr('id', `btn-${index - 7}`)
     }
-    if (index == moment().format('H')) {
-        $('textarea').eq(index-8).addClass('present')
+    if (index == moment().format('H')) {                                                                    //if-else statement for the bg color
+        $('textarea').eq(index - 8).addClass('present')
     } else if (index < moment().format('H')) {
-        $('textarea').eq(index-8).addClass('past')
+        $('textarea').eq(index - 8).addClass('past')
     } else {
-        $('textarea').eq(index-8).addClass('future')
+        $('textarea').eq(index - 8).addClass('future')
     }
+    $(`#text-0${index-7}`).text(localStorage.getItem(`text-0${index-7}`))                                   //retrieves the stored value
+
 }
+
+
+saveBtn.on('click', function () {
+    var buttonNbr = $(this.parentNode).children('.saveBtn').attr('id');                                     //gets the parent node id to know 
+    var textEl = $(this.parentNode).children('.col-md-10').attr('id');
+    var textToStorage = $(this.parentNode).children('.col-md-10').val();
+    localStorage.setItem(textEl, textToStorage)
+})
+
+
+
