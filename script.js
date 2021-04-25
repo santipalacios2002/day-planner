ef1b78caaf5f3f561a3e38eb6083179ff5ca55d0
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-25 00:28:28 -0400

    cleaned my code a bit more

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var saveBtn = $('.row').children('.saveBtn')
-// console.log(saveBtn)
+var saveBtn = $('.row').children('.saveBtn')                                            //retrieves all the buttons
+
 
 //=====================  Displays current day  =====================
 //=======================  in the jumbotron  =======================
 
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));                           //adds the current date in day, month, date, year format
 
 
 //=====================  Adds the hour to the time col and   =====================
@@ -12,29 +12,24 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 for (var index = 8; index < 17; index++) {
     if (index < 12) {
-        $('.row').children('.hour').eq(index - 8).text(`${index} AM`)
-        $('.row').children('.saveBtn').eq(index - 8).attr('id', `btn-${index - 7}`)
+        $('.row').children('.hour').eq(index - 8).text(`${index} AM`)                   //adds the time on the time col
     } else {
         $('.row').children('.hour').eq(index - 8).text(`${index} PM`)
-        $('.row').children('.saveBtn').eq(index - 8).attr('id', `btn-${index - 7}`)
     }
-    if (index == moment().format('H')) {                                                                    //if-else statement for the bg color
+    if (index == moment().format('H')) {                                                //if-else statement for the bg color
         $('textarea').eq(index - 8).addClass('present')
     } else if (index < moment().format('H')) {
         $('textarea').eq(index - 8).addClass('past')
     } else {
         $('textarea').eq(index - 8).addClass('future')
     }
-    $(`#text-0${index-7}`).text(localStorage.getItem(`text-0${index-7}`))                                   //retrieves the stored value
-
+    $(`#text-0${index - 7}`).text(localStorage.getItem(`text-0${index - 7}`))           //retrieves all the stored values and places them on their proper text element
 }
 
-
 saveBtn.on('click', function () {
-    var buttonNbr = $(this.parentNode).children('.saveBtn').attr('id');                                     //gets the parent node id to know 
-    var textEl = $(this.parentNode).children('.col-md-10').attr('id');
-    var textToStorage = $(this.parentNode).children('.col-md-10').val();
-    localStorage.setItem(textEl, textToStorage)
+    var textEl = $(this.parentNode).children('.col-md-10').attr('id');                  //gets the text element id from the text col by going to the parent node, down to the children 
+    var textToStorage = $(this.parentNode).children('.col-md-10').val();                //retrieves the text that will be stored locally
+    localStorage.setItem(textEl, textToStorage)                                         //stores the element and the text
 })
 
 
