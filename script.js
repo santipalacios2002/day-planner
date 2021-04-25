92b16809a71423d07f544f0964a162b8c55ef3ee
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-24 22:36:35 -0400

    added logic to the bg color of the text area and hour col

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
-//=====================  Adds the hour to  =====================
-//=======================  the time col  =======================
+//=====================  Adds the hour to the time col and   =====================
+//=================  adds bg color to past, present, and future  =================
 
 for (var index = 8; index < 17; index++) {
     if (index < 12) {
@@ -15,7 +15,11 @@ for (var index = 8; index < 17; index++) {
     } else {
         $('.row').children('.hour').eq(index-8).text(`${index} PM`)
     }
+    if (index == moment().format('H')) {
+        $('textarea').eq(index-8).addClass('present')
+    } else if (index < moment().format('H')) {
+        $('textarea').eq(index-8).addClass('past')
+    } else {
+        $('textarea').eq(index-8).addClass('future')
+    }
 }
-
-
-
